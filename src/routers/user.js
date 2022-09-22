6b217ba97ff24ee6d0c8c38487a916d6122c6752
nguye374 <nguye374@sheridancollege.ca>
2022-09-22 00:04:47 -0400

    create user routes

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,27 +1,12 @@
 const express = require('express');
 const User = require('../models/user');
 const router = new express.Router();
-const auth = require('../middlewares/auth');
-
-// router.post('/users', async (req, res) => {
-//   const user = new User(req.body);
-//   try {
-//     await user.save();
-//     const token = await user.generateAuthToken();
-//     res.status(201).send({ user, token });
-//   } catch (e) {
-//     res.status(400).send(e);
-//   }
-// });
 
 router.post('/users/create', async (req, res) => {
+  const user = new User(req.body);
   try {
-    const user = await User.findByCredentials(
-      req.body.email,
-      req.body.passcode
-    );
-    const token = await user.generateAuthToken();
-    res.send({ user, token });
+    await user.save();
+    res.status(201).send({ user });
   } catch (e) {
     res.status(400).send();
   }
@@ -33,7 +18,7 @@ router.post('/users/check', async (req, res) => {
       req.body.email,
       req.body.passcode
     );
-    // const token = await user.generateAuthToken();
+    user && res.status(200).send();
   } catch (e) {
     res.status(400).send();
   }
