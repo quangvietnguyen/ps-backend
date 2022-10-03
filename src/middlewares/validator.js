d02bd14ce07a9923bd456e18002cc6a7d0a29568
nguye374 <nguye374@sheridancollege.ca>
2022-10-03 00:55:59 -0400

    update validator

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -9,14 +9,16 @@ module.exports = async (req, res, next) => {
     if (req.method === 'POST' && !req.body.name) {
       throw new HttpError('There must be a name.', 401);
     }
-    if (!req.body.email) {
-      throw new HttpError('There must be an email.', 401);
-    }
-    if (!req.body.passcode) {
-      throw new HttpError('There must be a passcode.', 401);
-    }
-    if (!validator.isEmail(req.body.email)) {
-      throw new HttpError('Email is not valid.', 401);
+    if (req.method !== 'GET') {
+      if (!req.body.email) {
+        throw new HttpError('There must be an email.', 401);
+      }
+      if (!req.body.passcode) {
+        throw new HttpError('There must be a passcode.', 401);
+      }
+      if (!validator.isEmail(req.body.email)) {
+        throw new HttpError('Email is not valid.', 401);
+      }
     }
     next();
   } catch (err) {
--- src/routers/comment.js
+++ src/routers/comment.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = new express.Router();
 const commentsControllers = require('../controllers/comments-controllers');
 
-router.get('/comments/:post', commentsControllers.getComments);
+router.get('/api/comments/:post', commentsControllers.getComments);
 
-router.get('/comment/:id', commentsControllers.getComment);
+router.get('/api/comment/:id', commentsControllers.getComment);
 
-router.post('/comment/create', commentsControllers.createComment);
+router.post('/api/comment/create', commentsControllers.createComment);
 
-router.put('/comment/:id', commentsControllers.updateComment);
+router.put('/api/comment/:id', commentsControllers.updateComment);
 
-router.delete('/comment/:id', commentsControllers.deleteComment);
+router.delete('/api/comment/:id', commentsControllers.deleteComment);
 
 module.exports = router;
