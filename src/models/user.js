7648649862dce7c0f38af4f0190f054bc53b0c50
nguye374 <nguye374@sheridancollege.ca>
2022-10-07 00:05:22 -0400

    update deploy script

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const { Schema } = mongoose;
+const dotenv = require('dotenv');
+dotenv.config();
 
 const userSchema = new mongoose.Schema(
   {
